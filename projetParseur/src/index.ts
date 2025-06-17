06c006725f737157a6b0103b08e0c5058c3c6d29
ItsMii176 <mathias.boldron@etu.unistra.fr>
2025-06-17 16:12:12 +0200

    Commander.js features added
    Allows parsing an Excel file from the terminal by specifying its name. By default, 2A internships will be parsed.

--- projetParseur/src/index.ts
+++ projetParseur/src/index.ts
@@ -1,27 +1,38 @@
+import { Command } from "commander";
 import { parseExcelInternship2A } from "./parserInternship2A";
 import { parseExcelSubstitutionInternship } from "./parserSubstitutionInternship";
 import path from "path";
+
+const program = new Command();
+
+program
+  .option("-s, --sheet <name>", "Excel sheet name", "2A - Récap. stage")
+  .parse();
+
+const options = program.opts();
 
 async function main() {
   const fileName = "2024-25-Récapitulatif des stages 2A.xlsx";
   const filePath = path.join(process.cwd(), fileName);
 
-  // Stage de 2A
-  const sheetName = "2A - Récap. stage";
-  const { internships, students, organizations } = await parseExcelInternship2A(
-    filePath,
-    sheetName
-  );
-
-  // Stage de substitution
-  /*
-  const sheetName = "Stage substitution";
-  const { internships, students, organizations } = await parseExcelSubstitutionInternship(
-    filePath,
-    sheetName
-  );
-  */
-
+  // Utilise le nom de feuille passé en argument ou la valeur par défaut
+  const sheetName = options.sheet;
+  let internships, students, organizations;
+  switch (sheetName) {
+    case "2A - Récap. stage":
+      ({ internships, students, organizations } = await parseExcelInternship2A(
+        filePath,
+        sheetName
+      ));
+      break;
+    case "Stage substitution":
+      ({ internships, students, organizations } =
+        await parseExcelSubstitutionInternship(filePath, sheetName));
+      break;
+    default:
+      throw new Error(`Unknown sheet name: ${sheetName}`);
+  }
+  console.log(`Parsed data from sheet: ${sheetName}`);
   console.log("Internships:", internships);
   console.log("Students:", students);
   console.log("Organizations:", organizations);
