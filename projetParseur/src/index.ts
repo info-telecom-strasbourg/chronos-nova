5d781b6793e55799c5edd532d1d5832eb7bd6755
ItsMii176 <mathias.boldron@etu.unistra.fr>
2025-06-18 09:46:48 +0200

    New features added to the parser
    It is now possible to pass the name of the Excel file in the command line, followed by the name(s) of the worksheet(s) to be parsed.

--- projetParseur/src/index.ts
+++ projetParseur/src/index.ts
@@ -2,40 +2,44 @@ import { Command } from "commander";
 import { parseExcelInternship2A } from "./parserInternship2A";
 import { parseExcelSubstitutionInternship } from "./parserSubstitutionInternship";
 import path from "path";
+import fs from "fs";
 
 const program = new Command();
 
 program
-  .option("-s, --sheet <name>", "Excel sheet name", "2A - Récap. stage")
+  .option("-s, --sheet <names...>", "Excel name then sheet names", [])
   .parse();
 
 const options = program.opts();
 
 async function main() {
-  const fileName = "2024-25-Récapitulatif des stages 2A.xlsx";
+  const fileName = options.sheet[0];
   const filePath = path.join(process.cwd(), fileName);
-
-  // Utilise le nom de feuille passé en argument ou la valeur par défaut
-  const sheetName = options.sheet;
-  let internships, students, organizations;
-  switch (sheetName) {
-    case "2A - Récap. stage":
-      ({ internships, students, organizations } = await parseExcelInternship2A(
-        filePath,
-        sheetName
-      ));
-      break;
-    case "Stage substitution":
-      ({ internships, students, organizations } =
-        await parseExcelSubstitutionInternship(filePath, sheetName));
-      break;
-    default:
-      throw new Error(`Unknown sheet name: ${sheetName}`);
+  if (!fs.existsSync(filePath)) {
+    throw new Error(`File "${fileName}" not found: ${filePath}`);
   }
-  console.log(`Parsed data from sheet: ${sheetName}`);
-  console.log("Internships:", internships);
-  console.log("Students:", students);
-  console.log("Organizations:", organizations);
+  console.log(`Parsed data from file: "${fileName}"\n`);
+
+  for (let i = 1; i < options.sheet.length; i++) {
+    const sheetName = options.sheet[i];
+    let internships, students, organizations;
+    switch (sheetName) {
+      case "2A - Récap. stage":
+        ({ internships, students, organizations } =
+          await parseExcelInternship2A(filePath, sheetName));
+        break;
+      case "Stage substitution":
+        ({ internships, students, organizations } =
+          await parseExcelSubstitutionInternship(filePath, sheetName));
+        break;
+      default:
+        throw new Error(`Unknown sheet name: "${sheetName}"`);
+    }
+    console.log(`Parsed data from sheet: "${sheetName}"`);
+    console.log("Internships:", internships);
+    console.log("Students:", students);
+    console.log("Organizations:", organizations);
+  }
 }
 
 try {
