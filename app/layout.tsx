b8e52a66c32dc408ae115fdaedca64670fdc9478
Romain Bourdain <romain.bourdain93@gmail.com>
2025-06-20 14:56:24 +0200

    🎨 Implement basic layout files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Providers } from "@/components/providers";
 import "@/styles/globals.css";
 import type { Metadata } from "next";
+import { Footer } from "@/components/layout/footer";
+import { Navbar } from "@/components/layout/navbar";
 import type { LayoutParams } from "@/types/next";
 
 export const metadata: Metadata = {
@@ -12,7 +14,13 @@ export default function RootLayout({ children }: LayoutParams) {
   return (
     <html lang="fr" suppressHydrationWarning>
       <body className="antialiased">
-        <Providers>{children}</Providers>
+        <Providers>
+          <Navbar />
+          <main className="flex-1 mx-auto px-5 py-3 w-full max-w-screen-xl min-h-screen">
+            {children}
+          </main>
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import Image from "next/image";
-import { ThemeToggle } from "@/features/theme/theme-toggle";
 import type { PageParams } from "@/types/next";
 
 export default async function RoutePage(_: PageParams) {
-  return (
-    <div className="bg-primary">
-      Hello world
-      <Image alt="Chronos Logo" height={100} src="./logo.svg" width={100} />
-      <ThemeToggle />
-    </div>
-  );
+  return <div>Hello world</div>;
 }
