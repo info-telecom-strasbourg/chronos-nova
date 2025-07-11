8b98a2c9bdf25218521553fe611c442e288f0137
ItsMii176 <mathias.boldron@etu.unistra.fr>
2025-07-11 16:22:17 +0200

    :bug: Fix pluralization logic in internship count label

--- src/lib/scripts/string.ts
+++ src/lib/scripts/string.ts
@@ -1,3 +1,3 @@
 export const pluralize = (count: number, singular: string, plural: string): string => {
-  return `${count} ${count === 1 ? singular : plural}`;
+  return `${count} ${count <= 1 ? singular : plural}`;
 };
