017a186f6aedab6f0a877fc812d58346b67cad8c
fab-toc <81991643+fab-toc@users.noreply.github.com>
2026-05-13 19:32:44 +0200

    feat: update next config for admin app and packaging with Dockerfile

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -1,6 +1,10 @@
+import path from "node:path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactCompiler: true,
+  output: "standalone",
+  outputFileTracingRoot: path.join(__dirname, "../../"),
   transpilePackages: ["@chronos/ui", "@chronos/db", "@chronos/env"],
 };
 
